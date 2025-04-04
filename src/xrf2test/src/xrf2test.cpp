241163bc44c0d47dfe043611397268eb67cefc6a
thodoriskolios <thodoriskolios123@gmail.com>
2025-04-04 17:03:22 +0200

    final

--- src/xrf2test/src/xrf2test.cpp
+++ src/xrf2test/src/xrf2test.cpp
@@ -6,6 +6,17 @@
 #include <iostream>
 #include "xrf2_msgs/msg/ros2_xeno.hpp"
 
+// Struct to match ROS 2 Ros2Xeno message layout
+#pragma pack(push, 1)
+struct Ros2XenoRT {
+    int32_t sec;
+    uint32_t nanosec;
+    char frame_id[64];  // Max 63 chars + null terminator
+    double left_motor;
+    double right_motor;
+};
+#pragma pack(pop)
+
 xrf2test::xrf2test(uint write_decimator_freq, uint monitor_freq)
 : XenoFrt20Sim(write_decimator_freq, monitor_freq, file, &data_to_be_logged),
   file(1, "./xrf2_logging/XRF2TEST", "bin"),
@@ -46,11 +57,23 @@ int xrf2test::run()
     logger.start();
     monitor.printf("Hello from run\n");
 
-    // Toggle and modify log data
+    // ✅ NEW: Read motor command from ROS bridge via EVL
+    Ros2XenoRT msg;
+    ssize_t ret = read(data_fd, &msg, sizeof(msg));
+
+    if (ret > 0) {
+        u[0] = msg.left_motor;
+        u[1] = msg.right_motor;
+
+        monitor.printf("Received: L=%.2f, R=%.2f at %d.%09u (frame_id=%s)\n",
+                       msg.left_motor, msg.right_motor, msg.sec, msg.nanosec, msg.frame_id);
+    }
+
+    // Log data and simulate sensor state change
     data_to_be_logged.this_is_a_bool = !data_to_be_logged.this_is_a_bool;
     data_to_be_logged.this_is_a_int++;
 
-    if(data_to_be_logged.this_is_a_char == 'R')
+    if (data_to_be_logged.this_is_a_char == 'R')
         data_to_be_logged.this_is_a_char = 'A';
     else if (data_to_be_logged.this_is_a_char == 'A')
         data_to_be_logged.this_is_a_char = 'M';
@@ -62,7 +85,7 @@ int xrf2test::run()
 
     controller.Calculate(u, y);
 
-    if(controller.IsFinished())
+    if (controller.IsFinished())
         return 1;
 
     return 0;
